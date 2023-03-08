ef5d20b133e825ce3a59d3dd8bf9a9a1b60215b7
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-03-08 10:56:40 +0100

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Hossein Sharifi on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
